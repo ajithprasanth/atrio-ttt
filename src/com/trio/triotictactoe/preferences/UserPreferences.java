372c1cc7abe1eb33e3838bbd0d53f8b2dd8687a7
ajith <ajith@amazon.com>
2013-08-19 19:19:47 +0530

    Code clean up and adding facebook activity

--- src/com/trio/triotictactoe/preferences/UserPreferences.java
+++ src/com/trio/triotictactoe/preferences/UserPreferences.java
@@ -6,24 +6,18 @@ import android.content.SharedPreferences.Editor;
 
 public class UserPreferences implements Preferences {
 
-	
-	SharedPreferences pref;
+	private SharedPreferences pref;
 	public UserPreferences(Context context) {
-		pref = context.getSharedPreferences(context.getApplicationInfo().packageName, Context.MODE_PRIVATE );
+		pref = context.getSharedPreferences(context.getApplicationInfo().packageName, Context.MODE_PRIVATE);
 	}
-	
-	
-		
-	//	this.getSharedPreferences(
-		//      "com.example.app", Context.MODE_PRIVATE);
+
 	@Override
 	public void setPreference(String key, String value) {
 		Editor editor = pref.edit();
-		editor.putString(key,value);
-		
+		editor.putString(key, value);
+
 		editor.commit();
 		System.out.println(pref.getString(key, null));
-		// TODO Auto-generated method stub
 	}
 
 	@Override
