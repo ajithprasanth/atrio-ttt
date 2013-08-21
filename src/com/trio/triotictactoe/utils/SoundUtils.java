2d5e174294b559136bb982a19e19493e7af5b948
unknown <parasm@BLR2-PB4E968.ant.amazon.com>
2013-08-21 17:19:34 +0530

    Adding Sound and Vibration utils

--- src/com/trio/triotictactoe/utils/SoundUtils.java
+++ src/com/trio/triotictactoe/utils/SoundUtils.java
@@ -1,6 +1,8 @@
 package com.trio.triotictactoe.utils;
 
 import com.trio.triotictactoe.R;
+import com.trio.triotictactoe.preferences.Preferences;
+import com.trio.triotictactoe.preferences.UserPreferences;
 
 import android.content.Context;
 import android.media.AudioManager;
@@ -11,16 +13,28 @@ public class SoundUtils {
 
 	private int click;
 	private SoundPool pool;
-
+	private Vibrator vibrator;
+	private Preferences preferences;
+	
 	public SoundUtils(Context context) {
-		Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-		v.vibrate(100);
+		vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 		pool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
 		click = pool.load(context.getApplicationContext(), R.raw.click, 1);
+		preferences = new UserPreferences(context);
 	}
 
 	public void click() {
-		pool.play(click, 1, 1, 1, 0, 1);
+		if(!Boolean.parseBoolean(preferences.getPreference(TTTConstants.NO_SOUND, "false")))
+			pool.play(click, 1, 1, 1, 0, 1);
+		if(!Boolean.parseBoolean(preferences.getPreference(TTTConstants.NO_VIBRATOR, "false")))
+			vibrator.vibrate(100);
 	}
-
+	public void viberate(){
+		if(!Boolean.parseBoolean(preferences.getPreference(TTTConstants.NO_VIBRATOR, "false")))
+			vibrator.vibrate(100);
+	}
+	public void sound(){
+		if(!Boolean.parseBoolean(preferences.getPreference(TTTConstants.NO_SOUND, "false")))
+			pool.play(click, 1, 1, 1, 0, 1);
+	}
 }
